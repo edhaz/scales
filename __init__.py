ab75d527739789ad23dd958383800cdc6a42c9c3
Ed Hazledine <ed.hazledine@gmail.com>
2018-06-17 15:50:52 +0100

    Separate scales selection

--- __init__.py
+++ __init__.py
@@ -1,32 +1,54 @@
-import sqlite3
 import random
-from flask import Flask, redirect, render_template, request, session, g, url_for, jsonify
-from flask_session import Session
+
+from flask import Flask, redirect, render_template, request, session, url_for
+
+import select_scales
 
 app = Flask(__name__)
 
-#DATABASE = '/scales.db'
+"""
+DATABASE = '/scales.db'
 
-#def get_db():
+def get_db():
+    db = getattr(g, 'scales', None)
+    if db is None:
+        db = g.scales = sqlite3.connect(DATABASE)
+    db.row_factory = sqlite3.Row
+    return db
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+with app.app_context():
+    for scale in query_db('select * from scales'):
+        print(scale['name'], 'has', scale['octaves'], 'octaves')
+
+#@app.teardown_appcontext
+#def close_connection(exception):
 #    db = getattr(g, '_scales', None)
-#    if db is None:
-#        db = g._scales = sqlite3.connect(DATABASE)
-#    return db
+#    if db is not None:
+#        db.close()
+"""
 
 scales = []
+
 
 @app.route("/")
 def index():
     """Scales web app"""
-    
-    grades = ['4','5']
+
+    grades = ['4', '5']
 
     return render_template("scalesindex.html", grades=grades)
+
 
 @app.route("/redirecter", methods=['POST'])
 def redirecter():
     """Get list of scales"""
-    
+
     session['instrument'] = request.form.get('instrument')
     session['grade'] = request.form.get('grade')
 
@@ -35,18 +57,10 @@ def redirecter():
     elif not session['grade']:
         return redirect('/')
 
-    if session['instrument'] == 'violin':
-        if session['grade'] == '4':
-            session['scales'] = ['Ab major, 2 octaves', 'B major, 2 octaves', 'C major, 2 octaves', 'E major, 2 octaves', 'G minor, 2 octaves', 'B minor, 2 octaves', 'C minor, 2 octaves']
-        elif session['grade'] == '5':
-            session['scales'] = ['Db major', 'Eb major', 'F major', 'B minor', 'C# minor', 'E minor']
-    elif session['instrument'] == 'piano':
-        if session['grade'] == '4':
-            session['scales'] = ['test1', 'test2']
-        elif session['grade'] == '5':
-            session['scales'] = ['test1', 'test2']
+    session['scales'] = select_scales.get_scales(session['instrument'], session['grade'])
 
     return redirect(url_for('scales'))
+
 
 @app.route("/scales", methods=['GET', 'POST'])
 def scales():
@@ -54,9 +68,9 @@ def scales():
     instrument = session['instrument'].capitalize()
     grade = session['grade']
     try:
-        session['scales_tmp'] = session['scales']
-        scale = random.choice(session['scales_tmp'])
-        session['scales_tmp'].remove(scale)
+        #session['scales_tmp'] = session['scales']
+        scale = random.choice(session['scales'])
+        session['scales'].remove(scale)
         return render_template('scales.html', scale=scale, instrument=instrument, grade=grade)
     except IndexError:
         return render_template('finished.html', instrument=instrument, grade=grade)
