0b851a97279cd6d9d2207a1aff3ed00a1fc10b89
Ed Hazledine <ed.hazledine@gmail.com>
2018-12-16 10:53:27 +0000

    Update select_scales

--- app/select_scales.py
+++ app/select_scales.py
@@ -3,7 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 def get_scales(instrument, grade):
-    filename = basedir + '/scales.csv'
+    filename = os.path.join(basedir, 'scales.csv')
     scales = []
 
     rows = []
@@ -13,20 +13,33 @@ def get_scales(instrument, grade):
             rows.append(row)
 
     for row in rows:
-        name = row[2]
-        octaves = str(row[3])
-        if row[1] == grade and row[0] == instrument:
-            if octaves == 1 and row[4] != 'T':
-                scale = "{}, {} octave".format(name, octaves)
-                print(scale)
+        # define rows
+        csv_instrument = row[0]
+        csv_grade = row[1]
+        csv_name = row[2]
+        csv_octaves = row[3]
+        csv_arp = row[4]
+
+        if csv_grade == grade and csv_instrument == instrument:
+            if csv_arp == 'T':
+                if csv_octaves == 1:
+                    scale = "{}, {} octave".format(csv_name, csv_octaves)
+                    arp = "{} arpeggio, {} octave".format(csv_name, csv_octaves)
+                elif int(csv_octaves) > 1:
+                    scale = "{} scale, {} octaves".format(csv_name, csv_octaves)
+                    arp = "{} arpeggio, {} octaves".format(csv_name, csv_octaves)
                 scales.append(scale)
-                continue
-            elif int(octaves) > 1:
-                scale = "{} scale, {} octaves".format(name, octaves)
-                arp = "{} arpeggio, {} octaves".format(name, octaves)
-            else:
-                scale = "{} scale, {} octave".format(name, octaves)
-                arp = "{} arpeggio, {} octaves".format(name, octaves)
-            scales.append(scale)
-            scales.append(arp)
+                scales.append(arp)
+            elif csv_arp == 'F':
+                if csv_octaves == '1':
+                    scale = "{}, {} octave".format(csv_name, csv_octaves)
+                elif int(csv_octaves) > 1:
+                    scale = "{} scale, {} octaves".format(csv_name, csv_octaves)
+                scales.append(scale)
+
     return scales
+
+if __name__ == "__main__":
+    x = get_scales('violin', '4')
+    for i in x:
+        print(i)
