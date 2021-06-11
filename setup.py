67ddfc2edb6d9f6184d30902e52cc2928b9759d8
Ed Hazledine <ed.hazledine@gmail.com>
2021-06-11 21:28:48 +0100

    Initial setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='scales',
+      version='1.0',
+      description='Scales web app',
+      author='Ed Hazledine',
+      author_email='ed.hazledine@gmail.com',
+      packages=['scales'],
+      install_requires=[''],
+      )
